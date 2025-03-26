83f31d873ef38d539f837b8efe3d5321cbc00f7d
aviral <aviralgarg40@gmail.com>
2025-03-26 15:22:00 +0530

    bind 0.0.0.0

--- src/index.js
+++ src/index.js
@@ -6,10 +6,10 @@ dotenv.config({
 import express from "express";
 import { app } from "./app.js";
 
-import connectDB from "./db/index.js"; 
+import connectDB from "./db/index.js";
 connectDB()
   .then(() => {
-    app.listen(process.env.PORT || 8000, () => {
+    app.listen(process.env.PORT || 8000, "0.0.0.0", () => {
       console.log(`Server is running on port: ${process.env.PORT}`);
     });
   })
