1d245b538367d4eed914f148d542a9978de54476
aviral <aviralgarg40@gmail.com>
2026-06-25 20:14:13 +0530

    revisit

--- src/app.js
+++ src/app.js
@@ -1,6 +1,11 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import userRouter from "./routes/user.routes.js";
+import productRouter from "./routes/product.routes.js";
+import cartRouter from "./routes/cart.routes.js";
+import { errorHandler } from "./middlewares/errorHandler.middleware.js";
+
 
 const app = express();
 
@@ -16,19 +21,13 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
-import userRouter from "./routes/user.routes.js";
+app.get("/health", (req, res) => {
+  res.status(200).json({ ok: true });
+});
 
 app.use("/api/v2/users", userRouter);
-
-import productRouter from "./routes/product.routes.js";
-
 app.use("/api/v2/products", productRouter);
-
-import cartRouter from "./routes/cart.routes.js";
-import { errorHandler } from "./middlewares/errorHandler.middleware.js";
-
 app.use("/api/v2/cart", cartRouter);
-
 app.use(errorHandler);
 
 export { app };
