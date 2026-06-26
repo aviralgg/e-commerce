574aba4cde03631a80bf1eb0a7c0b2a702d79f71
aviral <aviralgarg40@gmail.com>
2026-06-26 22:30:31 +0530

    added status

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import connectDB from "./db/index.js";
 connectDB()
   .then(() => {
     app.get('/', (req, res) => {
-      res.send('Hello World!');
+      res.status(200).send('Hello World!');
     });
     app.listen(process.env.PORT || 8000, "0.0.0.0", () => {
       console.log(`Server is running on port: ${process.env.PORT}`);
