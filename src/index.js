af33384938ab5f027bcd06833f2077067b6e006c
aviral <aviralgarg40@gmail.com>
2026-06-29 23:46:50 +0530

    test webhook:1

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ connectDB()
     });
     app.listen(process.env.PORT || 8000, "0.0.0.0", () => {
       console.log(`Server is running on port: ${process.env.PORT}`);
+      console.log("Hola");
     });
   })
   .catch((err) => {
